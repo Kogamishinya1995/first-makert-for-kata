bbb14d64d5cc542b11723030484ac22b76bedeed
Kogamishinya1995 <79081103268@yandex.ru>
2024-08-30 13:06:09 +0300

    swiper-project-5

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-const elements = document.querySelectorAll('.menu__item');
-const showAllButton = document.querySelector('.show-all-button');
-const screenWidth = window.innerWidth;
+const elements = document.querySelectorAll('.menu__item'); 
+const showAllButton = document.querySelector('.show-all-button'); 
+const screenWidth = window.innerWidth; 
 
 
+let maximumVisibleElements; 
+    if (screenWidth < 1119 ) { 
+        maximumVisibleElements = 6;  
+    } 
+    else {  
+        maximumVisibleElements = 8;  
+    } 
 
-function hideElements() {
-    let maximumVisibleElements;
-    if (screenWidth < 1120 ) {
-        maximumVisibleElements = 6; 
-    }
-    else { 
-        maximumVisibleElements = 8; 
-    }
-    for (let i = 0; i < elements.length; i++) {
-        if (i >= maximumVisibleElements) {
-            elements[i].classList.add('hidden');
-        }
-    }
-};
+function hideElements() { 
+    for (let i = 0; i < elements.length; i++) { 
+        if (i >= maximumVisibleElements) { 
+            elements[i].classList.add('hidden'); 
+        } 
+    } 
+    showAllButton.textContent = 'Показать все'; 
+}; 
 
+function showElements () { 
+    for (let i = 0; i < elements.length; i++) { 
+        elements[i].classList.remove('hidden'); 
+    } 
+    showAllButton.textContent = 'Скрыть'; 
+} 
 
-hideElements();
-
+showAllButton.addEventListener('click', showElements); 
+hideElements(); 
--- js/swiper.js
+++ js/swiper.js
@@ -1,18 +1,32 @@
-new Swiper('.swiper', {
-    
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
-    spaceBetween: 30,
-    loop: true,
-    autoHeight: true,
-    width: 200,
-    centerInsufficientSlides: true,
-    breakpoints: {
-        768: {
-          enabled: false,
-        }
-      }
-});
+function swiperEnabled() { 
+  const screenWidth = window.innerWidth; 
+  if (screenWidth < 767) { 
+      const swiperInstance = new Swiper('.swiper', { 
+          pagination: { 
+              el: '.swiper-pagination', 
+              clickable: true, 
+              dynamicBullets: true, 
+          }, 
+          spaceBetween: 30, 
+          loop: true, 
+          autoHeight: true, 
+          width: 200, 
+          centerInsufficientSlides: true, 
+          breakpoints: { 
+              768: { 
+                  enabled: false, 
+              } 
+          } 
+      }); 
+
+      swiperEnabled.swiperInstance = swiperInstance; 
+  } else { 
+      if (swiperEnabled.swiperInstance) { 
+          swiperEnabled.swiperInstance.destroy(true, true); 
+          swiperEnabled.swiperInstance = null; 
+      } 
+  } 
+} 
+
+window.addEventListener("resize", swiperEnabled);  
+swiperEnabled(); 

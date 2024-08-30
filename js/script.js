cf8ffcb862e438cbe67ac9c15886676f03831f61
Kogamishinya1995 <79081103268@yandex.ru>
2024-08-30 14:23:55 +0300

    swiper-project-6

--- js/script.js
+++ js/script.js
@@ -20,12 +20,25 @@ function hideElements() {
     showAllButton.textContent = 'Показать все'; 
 }; 
 
-function showElements () { 
-    for (let i = 0; i < elements.length; i++) { 
-        elements[i].classList.remove('hidden'); 
-    } 
-    showAllButton.textContent = 'Скрыть'; 
-} 
+function showElements() {
+    if (showAllButton.textContent === 'Показать все') {
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('hidden');
+      }
+      showAllButton.textContent = 'Скрыть';
+      showAllButton.classList.add('show-hidden');
+    } else {
+      for (let i = 0; i < elements.length; i++) {
+        if (i >= maximumVisibleElements) {
+          elements[i].classList.add('hidden');
+        }
+      }
+      showAllButton.textContent = 'Показать все';
+      showAllButton.classList.remove('show-hidden');
+    }
+  }
+  
+
 
 showAllButton.addEventListener('click', showElements); 
 hideElements(); 

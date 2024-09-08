d24c329e66b37108de41c94421c3676d18cd58ee
Kogamishinya1995 <79081103268@yandex.ru>
2024-09-08 17:43:36 +0300

    final maket 17

---
+++ src/js/swiper-3.js
@@ -0,0 +1,35 @@
+export default function swiper3Enabled() { 
+  const screenWidth = window.innerWidth; 
+  if (screenWidth < 767) { 
+      const swiperInstance3 = new Swiper('.swiper-3', { 
+          pagination: { 
+              el: '.swiper-pagination', 
+              clickable: true, 
+              dynamicBullets: true, 
+              type: 'bullets',
+          }, 
+          spaceBetween: 30, 
+          loop: false, 
+          autoHeight: true, 
+          width: 200, 
+          centerInsufficientSlides: true, 
+          breakpoints: { 
+              768: { 
+                  enabled: false, 
+              } 
+          } 
+      }); 
+      
+      
+
+      swiper3Enabled.swiperInstance3 = swiperInstance3; 
+  } else { 
+      if (swiper3Enabled.swiperInstance3) { 
+          swiper3Enabled.swiperInstance3.destroy(true, true); 
+          swiper3Enabled.swiperInstance3 = null; 
+      } 
+  } 
+} 
+
+window.addEventListener("resize", swiper3Enabled);  
+swiper3Enabled(); 

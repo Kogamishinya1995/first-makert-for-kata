5c94597317d11c0e931e460db6d3e788243723a6
Kogamishinya1995 <79081103268@yandex.ru>
2024-09-14 10:19:28 +0300

    2 edition 4

--- src/js/repair-menu-button.js
+++ src/js/repair-menu-button.js
@@ -3,15 +3,9 @@ export default function showAllItems() {
   const repairMenu = document.querySelector('.repair-menu');
 
   repairMenuButton.addEventListener('click', () => {
-    if (repairMenuButton.textContent === 'скрыть') {
-      repairMenu.classList.add('repair-menu--hidden');
-      repairMenuButton.classList.remove('show-hidden');
-      repairMenuButton.textContent = 'Показать всё';
-    } else {
-      repairMenu.classList.remove('repair-menu--hidden');
-      repairMenuButton.textContent = 'скрыть';
-      repairMenuButton.classList.add('show-hidden');
-    }
+    repairMenu.classList.toggle('repair-menu--hidden');
+    repairMenuButton.classList.toggle('show-hidden');
+    repairMenuButton.textContent = repairMenu.classList.contains('repair-menu--hidden') ? 'Показать всё' : 'скрыть';
   });
 }
 
